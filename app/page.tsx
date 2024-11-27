49baa1b4833c3606f2b7b08559b9db5d83117d29
Ethan Tang <ethantang2026@gmail.com>
2024-11-27 16:21:16 -0700

    adding basebone authentication elements
    todo next: add DB logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
-import '../styles/page_home.css'
+import '@/styles/globals.css'
+import '@/styles/page_home.css'
 
 export default function Home() {
   return (
     <div className="home-page-bg">
-      <div className="flex-col-centered">
-
+      lol
+      <div className="flex-row-centered">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
       </div>
     </div>
   );
